6871843f2c5e3f6f61ce64aa6a3c63b00b75bcfe
RodrigoCosta <rfcosta85@gmail.com>
2024-02-21 14:15:52 +0000

    Implementa a rota

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,23 @@
-const express = require('express');
-const app = require('../server/server'); // Modify path if needed
+import express from 'express'
+import login from '../controllers/login.js';
+import logout from '../controllers/logout.js';
+import isAuthentication from '../controllers/is-authentication.js';
+import isAuthorization from '../controllers/is-authorization.js';
+import middlewareAuthentication from '../middlewares/middleware-is-autentication.js';
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+const authRouters = express.Router();
+
+authRouters.get('/', (req, res) => {
+    res.send('spotnotifier');
 });
+
+authRouters.get('/login', login);
+
+authRouters.get('/logout', middlewareAuthentication, logout);
+
+authRouters.get('/is-authentication', isAuthentication);
+
+authRouters.get('/is-authorization', middlewareAuthentication ,isAuthorization);
+
+export default authRouters;
+
